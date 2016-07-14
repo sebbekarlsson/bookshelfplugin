117684c0e9ce69cd39e910c44e7c23426b95182b
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2016-07-14 21:10:11 +0200

    Removed temporary error reporting

--- index.php
+++ index.php
@@ -10,8 +10,6 @@ License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 defined('ABSPATH') or die('Do not hack!');
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 
 require_once 'includes/load.php';
